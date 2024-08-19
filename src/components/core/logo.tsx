8127292b93766c3cb8e97a8d48d951c93d5652d1
n0vic30926 <m202271759@hust.edu.cn>
2024-08-19 03:53:13 +0000

    cancel the dynamiclogo

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -68,19 +68,19 @@ export function Logo({ color = 'light', emblem, height = HEIGHT, width = WIDTH }
   );
 }
 
-export function DynamicLogo({
-  colorDark = 'light',
-  colorLight = 'dark',
-  height = HEIGHT,
-  width = WIDTH,
-  ...props
-}: DynamicLogoProps): React.JSX.Element {
-  const { colorScheme } = useColorScheme();
-  const color = colorScheme === 'dark' ? colorDark : colorLight;
+// export function DynamicLogo({
+//   colorDark = 'light',
+//   colorLight = 'dark',
+//   height = HEIGHT,
+//   width = WIDTH,
+//   ...props
+// }: DynamicLogoProps): React.JSX.Element {
+//   const { colorScheme } = useColorScheme();
+//   const color = colorScheme === 'dark' ? colorDark : colorLight;
 
-  return (
-    <NoSsr fallback={<Box sx={{ height: `${height}px`, width: `${width}px` }} />}>
-      <Logo color={color} height={height} width={width} {...props} />
-    </NoSsr>
-  );
-}
+//   return (
+//     <NoSsr fallback={<Box sx={{ height: `${height}px`, width: `${width}px` }} />}>
+//       <Logo color={color} height={height} width={width} {...props} />
+//     </NoSsr>
+//   );
+// }
